1b3926f45b55b93dd91c0a57f7efa1f0365c52d8
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-20 07:12:08 +0530

    remove some modules

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import csv
 import wolframalpha
 import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
-import requests
 from csv import reader
 
-#west_coast_states = ['Colorado', 'Utah', 'Nevada', 'New Mexico', 'Idaho', '']
-#wc_states = requests.get('https://www.law.cornell.edu/definitions/uscode.php?width=840&height=800&iframe=true&def_id=43-USC-64403383-1554264330&term_occur=999&term_src=title:43:chapter:35:subchapter:I:section:1702').text
-#soup_wc = BeautifulSoup(wc_states, 'lxml')
 input_wolfram_wc = "What are the West Coast states of the U.S.?"
 app_id = "UY8J28-KPKYW5Y2RT"
 client = wolframalpha.Client(app_id)
@@ -45,6 +40,6 @@ y.append(counter2)
 plt.bar(x, y, color = 'g', width = 0.72, label = "Distribution")
 plt.xlabel('Coasts')
 plt.ylabel('Officials (Presidents and Vice Presidents)')
-plt.title('Distribution of US Govt. Officials with birthplaces either in West Coast or East Coast.')
+plt.title('Distribution of US Govt. Officials with birthplaces either in West Coast or East Coast')
 plt.legend()
 plt.show()
