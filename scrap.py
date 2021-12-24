34f993071dfc48219aa99bc8d673c1fd92cae125
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-24 07:54:16 +0530

    update

--- scrap.py
+++ scrap.py
@@ -13,10 +13,5 @@ linkz = [WikipediaPolitician.getLinkFromName(x) for x in ids]
 for link in linkz:
     data = requests.get(link).text
     soup = BeautifulSoup(data, 'lxml')
-    for title in soup.findAll('div', {"id":"mw-pages"}):
-        names = title.find_all('li')
-        for i in range(len(names)):
-            if names_21[i].get_text() in ids:
-                pass #here we have to code
 
     
