f91db3b1788900db961370d97a3bb735298c7368
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-24 07:45:39 +0530

    update

--- scrap.py
+++ scrap.py
@@ -7,12 +7,16 @@ from WikipediaClasses import WikipediaPolitician
 
 data = pd.read_csv(r'details.csv')
 ids = list(data['Politician'])
-print(ids)
 
 linkz = [WikipediaPolitician.getLinkFromName(x) for x in ids]
 
 for link in linkz:
     data = requests.get(link).text
     soup = BeautifulSoup(data, 'lxml')
+    for title in soup.findAll('div', {"id":"mw-pages"}):
+        names = title.find_all('li')
+        for i in range(len(names)):
+            if names_21[i].get_text() in ids:
+                pass #here we have to code
 
     
