de846b6b88dc947d660df62777e56e2d4aae99e3
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-25 07:18:03 +0530

    update

--- scrap.py
+++ scrap.py
@@ -4,18 +4,25 @@ import csv
 from csv import writer
 import pandas as pd
 from WikipediaClasses import WikipediaPolitician
-
+import wolframalpha
 data = pd.read_csv(r'details.csv')
 ids = list(data['Politician'])
 #print(ids)
 
 linkz = [WikipediaPolitician.getLinkFromName(x) for x in ids]
-print(linkz)
+#print(linkz)
 
 for link in linkz:
     data = requests.get(link).text
     soup = BeautifulSoup(data, 'lxml')
-    occupation = soup.findAll('div', class_='hlist hlist-separated')
-    print(occupation[-1])
-    print("============================================================")
-    
+    #occupation = soup.findAll('div', class_='hlist hlist-separated')
+    #print(occupation.parent.get_text())
+    #print("============================================================")
+    for official in ids:
+        input_wolfram = "What were the occupations of {}?".format(official)
+        app_id = "UY8J28-KPKYW5Y2RT"
+        client = wolframalpha.Client(app_id)
+        result = client.query(input_wolfram)  
+        answer = next(result.results).text  
+        print(answer)
+        print("==============================")
