62e4fa7e3625c9e12e9b79c7d7a2159eb92a3157
SentientPlatypus <trexxxxxxxxxxy@gmail.com>
2021-12-24 20:56:59 -0500

    added counter

--- scrap.py
+++ scrap.py
@@ -5,12 +5,17 @@ from csv import writer
 import pandas as pd
 from WikipediaClasses import WikipediaPolitician
 import wolframalpha
+from collections import Counter
+
+
 data = pd.read_csv(r'details.csv')
 ids = list(data['Politician'])
 #print(ids)
 
 linkz = [WikipediaPolitician.getLinkFromName(x) for x in ids]
 #print(linkz)
+
+counter = Counter()
 
 for link in linkz:
     data = requests.get(link).text
@@ -25,4 +30,7 @@ for link in linkz:
         result = client.query(input_wolfram)  
         answer = next(result.results).text  
         print(answer)
+        list = answer.split(" | ")
+        counter.update(list)
+        print(counter)
         print("==============================")
